4d993c8b7b06b56b5bb00f0334627ec0e3d249f1
Atlantis <184443362+Lantea-Git@users.noreply.github.com>
2026-07-30 01:11:42 +0200

    Add files via upload

---
+++ Chrome_Disable_AI.py
@@ -0,0 +1,21 @@
+import json
+import os
+import time
+
+
+
+policy_dir = "/etc/opt/chrome/policies/managed"
+policy_file = os.path.join(policy_dir, "chrome_policies.json")
+
+policies = {
+    "LensOverlaySettings": 1,
+    "SearchContentSharingSettings": 1
+    "AIModeSettings": 1
+}
+content = json.dumps(policies, indent=2)
+
+os.system(f"sudo mkdir -p {policy_dir}")
+os.system(f"echo '{content}' | sudo tee {policy_file}")
+
+print(f"Politiques écrites dans {policy_file}")
+time.sleep(1)
